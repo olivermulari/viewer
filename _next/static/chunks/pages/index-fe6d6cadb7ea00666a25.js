3c72ca96966f33fa53a8e47cc84d9b718434a7a7
Oliver Mulari <oliver@feelbie.com>
2021-06-08 12:59:52 +0300

    Updates

--- _next/static/chunks/pages/index-fe6d6cadb7ea00666a25.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8474:function(n,u,_){"use strict";_.r(u);var t=_(90);u.default=t.default},8581:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(8474)}])}},function(n){n.O(0,[90,774,888,179],(function(){return u=8581,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
